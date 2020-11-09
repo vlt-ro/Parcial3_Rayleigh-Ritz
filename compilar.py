d987b63ec800530b108b914bb4ddc1f30e70c24f
Valentina Roquemen <valentina.roquemen@udea.edu.co>
2020-11-09 10:41:57 -0500

    fix: compilation file

--- compilar.py
+++ compilar.py
@@ -2,4 +2,4 @@
 import os
 import sys
 
-os.system( 'g++ example_variacional.cpp rayleighritz/linearrayleighritz/linearrayleighritz.cpp rayleighritz/csrayleighritz/csrayleighritz.cpp rayleighritz/csrayleighritz/bspline.cpp')
+os.system( 'g++ main.cpp rayleighritz/linearrayleighritz/linearrayleighritz.cpp rayleighritz/csrayleighritz/csrayleighritz.cpp rayleighritz/csrayleighritz/bspline.cpp')
